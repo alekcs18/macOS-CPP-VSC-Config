b0c2b37b6eceabe3dacacd86f1e0a5bc30cd1777
alekcs18 <alex10092007@outlook.com>
2026-04-28 14:46:21 +0300

    Use of string

--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 //use shift+comand+r to run the code
 
 #include <iostream>
-
-
+#include <string>
+#include <vector>
 using namespace std;
 
-int main(){
-    cout<<"Setup!"<<endl;
-    
+
+
+int main(){    //012345678
+    string a = {"is string"};
+    cout<<a.size()<<endl;
+    cout<<a.capacity()<<endl;
+    for(int i = 0; i <a.size() - 8; ++i){
+        cout<<a[1];
+    }
     return 0;
 }
