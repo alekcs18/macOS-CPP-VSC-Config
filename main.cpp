66c1f8650a66e34f092fd63908ce5c3b54a9fb0d
alekcs18 <alex10092007@outlook.com>
2026-04-28 22:51:57 +0300

    Integrated a matrix function to implement it in 3D

--- main.cpp
+++ main.cpp
@@ -8,10 +8,7 @@ using namespace std;
 
 
 
-int main(){    
-    vector<int> v1 = {1, 2, 3};
-    vector<int> v2 = {4, 5, 6};
-    vector<int> v3 = {7, 8, 9};
+void matrix(vector<int> v1, vector<int> v2,vector<int> v3){
     cout<<"(";
     for(int x : v1){
         cout<< x << " ";
@@ -34,7 +31,16 @@ int main(){
     cout<<")";
 
     cout<<endl;
-    
+}
+
+
+int main(){    
+    vector<int> v1 = {1, 2, 3};
+    vector<int> v2 = {4, 5, 6};
+    vector<int> v3 = {7, 8, 9};
+    matrix(v1, v2, v3);
+    cout<<endl;
+    matrix(v3, v2, v1);
     return 0;
     
 
