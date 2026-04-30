151791661864a1d8e198cf70802b0c74c437981f
alekcs18 <alex10092007@outlook.com>
2026-04-30 22:11:28 +0300

    Simulator Veceu 1.0

--- main.cpp
+++ main.cpp
@@ -41,11 +41,21 @@ class Caca : public nevoiUmane{
 
 int main(){    
 
+
+
+int scoreboardCaca = 0;
+int scoreboardPipi = 0;
+
 bool trebuieSaFac = true;
 while(trebuieSaFac){
+cout<<"De cate ori ati facut caca: "<<scoreboardCaca<<endl;
+cout<<"De cate ori ati facut pipi: "<<scoreboardPipi<<endl;
+
 int choice = 0;
-cout<<"Alegeti ce vreti sa faceti: \n"<<"1.Pipi \n"<<"2. Caca \n";
+cout<<"Alegeti ce vreti sa faceti: \n"<<"1.Pipi \n"<<"2. Caca \n"<<"3. Exit \n";
 cin>>choice; 
+
+
 if(choice == 1){ 
 Pipi amFacutPipi = Pipi();
 
@@ -53,9 +63,8 @@ vector<Pipi> facPipi;
 facPipi.push_back(amFacutPipi);
 for(int i = 0; i < facPipi.size(); i++){
     facPipi[i];
-    
+    scoreboardPipi++;
 }
-break;
 }
 if(choice == 2){ 
 Caca amFacutCaca = Caca();
@@ -64,15 +73,37 @@ vector<Caca> facCaca;
 facCaca.push_back(amFacutCaca);
 for(int j = 0; j < facCaca.size(); j++){
     facCaca[j];
-    
-}
-break;
+    scoreboardCaca++;
 }
 
+}
+if(choice == 3){
+    cout<<"Thank you for playing Veceu Simulator!"<<endl;
+    return 0;
 
+}
+
+char playAgain;
+do{
+cout << "Do you want to play again?(y/n)\n";
+            cin >> playAgain;
+            if (playAgain == 'n' || playAgain == 'N') // this is no which means the game will end and program will return 0
+            {
+                trebuieSaFac = false;
+                break;
+            }
+            if (playAgain == 'y' || playAgain == 'Y') // this will cause another round to start
+            {
+                trebuieSaFac = true;
+                break;
+            }
+            cout << "Invalid choice! please choose from (y/n)\n"; // If player's choice is invalid it will display this message
+        
+}while(true);
 
 
 }
+
+cout<<"Program is exiting.."<<endl;
 return 0;
-
 }
