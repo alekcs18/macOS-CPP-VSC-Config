5cafe412271578a8bf58c629b755df73b8a6d09f
alekcs18 <alex10092007@outlook.com>
2026-05-01 00:01:20 +0300

    Some set exercise

--- main.cpp
+++ main.cpp
@@ -7,114 +7,18 @@
 #include <memory>
 #include <array>
 #include <limits>
+#include <fstream>
 using namespace std;
-
-
-class nevoiUmane{
-    private:
-    int obiectNevoi;
-    public:
-    nevoiUmane(){
-        cout<<"exista o nevoie: "<<endl;
-    }
-    virtual ~nevoiUmane(){};
-};
-
-
-
-
-class Pipi : public nevoiUmane{
-    public:
-    Pipi(){
-        cout<<"am facut pipi"<<endl;
-    };
-    ~Pipi(){};
-};
-class Caca : public nevoiUmane{
-    public:
-    Caca(){
-        cout<<"am facut caca"<<endl;
-    };
-    ~Caca(){};
-};
 
 
 
 int main(){    
+    ifstream file("input.txt");
+    string line;
 
-
-int scoreboardCaca = 0;
-int scoreboardPipi = 0;
-
-bool trebuieSaFac = true;
-while(trebuieSaFac){
-cout<<"\nBine ati venit la veceu simulator!\n\n";
-
-cout<<"\n ~~~ScoreBoard~~~~\n";
-cout<<"\nDe cate ori ati facut caca: "<<scoreboardCaca<<"\n";
-cout<<"\nDe cate ori ati facut pipi: "<<scoreboardPipi<<"\n";
-
-int choice = 0;
-cout<<"\nAlegeti ce vreti sa faceti: \n1. Pipi \n2. Caca \n3. Exit \nChoice: ";
-
-while(true){
-if(!(cin>>choice))
-{
-    cout<<"You can only choose from: 1 ; 2 ; 3"<<endl;
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    continue;
-}
-
-if(choice == 1){ 
-Pipi amFacutPipi = Pipi();
-
-vector<Pipi> facPipi;
-facPipi.push_back(amFacutPipi);
-for(int i = 0; i < facPipi.size(); i++){
-    facPipi[i];
-    scoreboardPipi++;
-}
-break;
-}
-if(choice == 2){ 
-Caca amFacutCaca = Caca();
-
-vector<Caca> facCaca;
-facCaca.push_back(amFacutCaca);
-for(int j = 0; j < facCaca.size(); j++){
-    facCaca[j];
-    scoreboardCaca++;
-}
-break;
-}
-if(choice == 3){
-    cout<<"Thank you for playing Veceu Simulator!"<<endl;
-    return 0;
-
-}
-}
-char playAgain;
-do{
-cout << "Do you want to play again?(y/n)\n";
-            cin >> playAgain;
-            if (playAgain == 'n' || playAgain == 'N') // this is no which means the game will end and program will return 0
-            {
-                trebuieSaFac = false;
-                break;
-            }
-            if (playAgain == 'y' || playAgain == 'Y') // this will cause another round to start
-            {
-                trebuieSaFac = true;
-                break;
-            }
-            cout << "Invalid choice! please choose from (y/n)\n"; // If player's choice is invalid it will display this message
-        
-}while(true);
-
-
-}
-
-cout<<"Program is exiting.."<<endl;
+    while(getline(file, line)){
+        //split line in word
+        //insert each word in a set
+    }
 return 0;
 }
