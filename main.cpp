30b15b8e2bdc6c9f51499178c1a08acdaed2d8a1
alekcs18 <alex10092007@outlook.com>
2026-04-30 15:30:49 +0300

    Testing of c++ array basic functionalities and syntax

--- main.cpp
+++ main.cpp
@@ -5,53 +5,33 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <array>
 using namespace std;
 
-class Textura{
 
-public:
-Textura(){cout<<"Loaded"<<endl;};
-virtual ~Textura(){cout<<"Unloaded"<<endl;};
-
-
-
-};
-
-
-class OakTreeTextura : public Textura{
-
-public:
-OakTreeTextura(){};
-~OakTreeTextura(){};
+// class MyClass{
+//     public:
+//     MyClass(){
+//         cout<<" Constructed "<<endl;
+//     };
+//     ~MyClass(){
+//         cout<<" Destroyed "<<endl;
+//     };
 
 
-};
+// };
 
-
-// int someFunction(make_shared<MyClass> &somePtr){
-//     if(*somePtr == 0){
-//         cout<<"Container is empty";
-//         return 0;
-//     } else{
-//         return *somePtr;
-//     }
-
-// }
-
+void printData(array<unsigned int, 97> alex){
+    cout<<"Current number of kilograme: "<<alex.size()<<endl;
+}
 
 int main(){    
-// auto ptr = make_unique<int>();
-// auto ptr1 = make_shared<int>(); //continue at 1:16:00 smart ptr
-auto ptr = make_shared<OakTreeTextura>();
-OakTreeTextura tree = OakTreeTextura();
-OakTreeTextura leafs = OakTreeTextura();
+array<unsigned int, 97> alex;
+cout<<alex[1]<<endl;
 
-vector<shared_ptr<Textura>> vecTexture;
-vecTexture.push_back(ptr);
-for(int i = 0; i < vecTexture.size(); i++){
-    cout<<"Vector stored textures: "<<vecTexture[i]<<endl;
-}
-cout<<"Are stored: "<<ptr.use_count()<<endl;
+printData(alex);
+cout<<alex.max_size()<<endl;
+
 
 
 return 0;
